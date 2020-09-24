d9a1e78e77162442f21d3aa29137cf9595744c26
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-09-23 22:31:06 -0700

    :sparkles: Begin command line interface implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+/// Simple Git implementation with Rust.
+
+use clap::Clap;
+
+/// Basic implmentation of Git.
+#[derive(Clap)]
+#[clap(version = "0.0.1", author = "Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>")]
+struct Opts {
+    /// Increase the verbosity of messages
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: u8,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    /// Create an empty RGit repository
+    Init,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Init => println!("Hello world!"),
+    }
 }
